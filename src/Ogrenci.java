8d21fc51bf7a7363bdd480c7bff80d6293d021d7
Samet <samettansel.amazon@gmail.com>
2022-10-01 14:02:10 +0300

    Öğrenci Classı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba Dünya");
+
+
+        System.out.println("MErhaba dünya 3");
+        System.out.println("Merhaba dünya 2");
     }
 }
